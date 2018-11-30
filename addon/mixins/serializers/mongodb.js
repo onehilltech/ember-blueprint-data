4e938e1b10dfeca5a688950fc2c76b183d177479
James H. Hill <hilljh82@gmail.com>
2018-11-29 20:53:51 -0500

    feat: Replace the pluralize response with a singular response

--- addon/mixins/serializers/mongodb.js
+++ addon/mixins/serializers/mongodb.js
@@ -1,10 +1,23 @@
 import Mixin from '@ember/object/mixin';
 import { underscore } from '@ember/string';
 
+import { singularize, pluralize } from 'ember-inflector';
+
 export default Mixin.create ({
   primaryKey: '_id',
 
   keyForAttribute: function (key) {
     return underscore (key);
+  },
+
+  normalizeQueryRecordResponse (store, primaryModelClass, payload, id, requestType) {
+    let plural = pluralize (primaryModelClass.modelName);
+    let singular = singularize (primaryModelClass.modelName);
+
+    payload[singular] = payload[plural][0];
+
+    delete payload[plural];
+
+    return this._super (store, primaryModelClass, payload, id, requestType);
   }
 });
