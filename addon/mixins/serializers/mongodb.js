e1182255adf93e698698593fd73e061524f9dfde
James H. Hill <hilljh82@gmail.com>
2019-01-04 15:39:20 -0500

    fix: Remove check to exclude relationship if changed

--- addon/mixins/serializers/mongodb.js
+++ addon/mixins/serializers/mongodb.js
@@ -57,13 +57,6 @@ export default Mixin.create ({
    */
   serializeBelongsTo (snapshot, json, relationship) {
     let key = relationship.key;
-
-    const changed = snapshot.changedAttributes ();
-
-    if (isNone (changed[key])) {
-      return;
-    }
-
     let belongsTo = snapshot.belongsTo (key);
     key = this.keyForRelationship ? this.keyForRelationship (key, "belongsTo", "serialize") : key;
 
