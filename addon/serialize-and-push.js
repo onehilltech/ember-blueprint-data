2947c8e59564c68cad6699562290de58c951aac7
James Hill <james@onehilltech.com>
2021-01-17 22:59:58 -0500

    fix: did not honer the provided model name

--- addon/serialize-and-push.js
+++ addon/serialize-and-push.js
@@ -1,8 +1,8 @@
 export default function serializeAndPush (options = {}) {
   return function (response) {
-    const modelClass = this.constructor;
-    const { model: modelName = modelClass.modelName, requestType } = options;
+    const { model: modelName = this.constructor.modelName, requestType } = options;
     const serializer = this.store.serializerFor (modelName);
+    const modelClass = this.store.modelFor (modelName) || this.constructor;
 
     let normalized = serializer.normalizeResponse (this.store, modelClass, response, null, requestType);
 
