db656d88f4d906fb084b64e960184b4e90f7c6c8
James Hill <james@onehilltech.com>
2023-01-18 09:56:28 -0500

    chore: updated to @ember-data 3.28

--- addon/-private/actions/search.js
+++ addon/-private/actions/search.js
@@ -4,11 +4,10 @@ import { normalizeResponseHelper } from './serializer-response';
 import { serializerForAdapter } from './serializers';
 
 export default function (adapter, store, modelName, query, recordArray, options, directives) {
-
-  let modelClass = store.modelFor (modelName); // adapter.search needs the class
+  const modelClass = store.modelFor (modelName); // adapter.search needs the class
 
   let promise;
-  let createRecordArray = adapter.search.length > 3 || (adapter.search.wrappedFunction && adapter.search.wrappedFunction.length > 3);
+  const createRecordArray = adapter.search.length > 3 || (adapter.search.wrappedFunction && adapter.search.wrappedFunction.length > 3);
 
   if (createRecordArray) {
     recordArray = recordArray || store.recordArrayManager.createAdapterPopulatedRecordArray (modelName, query);
@@ -22,46 +21,37 @@ export default function (adapter, store, modelName, query, recordArray, options,
   //promise = guardDestroyedStore(promise, store, label);
 
 
-  return promise.then(
-    adapterPayload => {
-      //let serializerToken = heimdall.start('initial-serializerFor-lookup');
-      let serializer = serializerForAdapter (store, adapter, modelName);
-      //heimdall.stop(serializerToken);
-      //let normalizeToken = heimdall.start('finders#_query::normalizeResponseHelper');
-      let payload = normalizeResponseHelper(
-        serializer,
-        store,
-        modelClass,
-        adapterPayload,
-        null,
-        'query'
-      );
-      //heimdall.stop(normalizeToken);
-      let internalModels = store._push(payload);
+  return promise.then (adapterPayload => {
+    const serializer = serializerForAdapter (store, adapter, modelName);
+    const payload = normalizeResponseHelper(serializer, store, modelClass, adapterPayload, null,'query');
+    const internalModels = store._push(payload);
 
-      assert(
-        'The response to store.query is expected to be an array but it was a single record. Please wrap your response in an array or use `store.queryRecord` to query for a single record.',
-        Array.isArray(internalModels)
-      );
+    assert(
+      'The response to store.search is expected to be an array but it was a single record. Please wrap your response in an array.',
+      Array.isArray(internalModels)
+    );
 
-      if (recordArray) {
-        // _setInternalModels is for older version of @ember-data and _setObjects is for
-        // newer versions of @ember-data.
-        const setter = recordArray._setInternalModels || recordArray._setObjects;
-        setter.call (recordArray, internalModels, payload);
+    const identifiers = internalModels.map (im => im.identifier);
+
+    if (recordArray) {
+      if (recordArray._setIdentifiers) {
+        // This is for @ember-data 3.28 and greater.
+        recordArray._setIdentifiers (identifiers, payload);
       }
       else {
-        recordArray = store.recordArrayManager.createAdapterPopulatedRecordArray(
-          modelName,
-          query,
-          internalModels,
-          payload
-        );
+        // _setInternalModels is for older version of @ember-data.
+        recordArray._setInternalModels(internalModels, payload);
       }
+    }
+    else {
+      recordArray = store.recordArrayManager.createAdapterPopulatedRecordArray(
+        modelName,
+        query,
+        internalModels, // This will need to be identifiers in later versions of @ember-data
+        payload
+      );
+    }
 
-      return recordArray;
-    },
-    null,
-    `DS: Extract payload of search ${modelName}`
-  );
+    return recordArray;
+  }, null, `DS: Extract payload of search ${modelName}`);
 }
