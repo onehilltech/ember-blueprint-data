86b9198345756200c4b698e0016a17ab50df8780
James Hill <hilljh82@gmail.com>
2024-04-09 12:41:52 -0400

    fix: failed to update blueprint

--- blueprints/ember-blueprint-data/index.js
+++ blueprints/ember-blueprint-data/index.js
@@ -4,6 +4,6 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
 module.exports = Blueprint.extend ({
   addons: [
-    { name: 'ember-data-model-fragments', target: '^5.0.0-beta.5' },
+    { name: 'ember-data-model-fragments', target: '^6.0.0' },
   ]
 });
