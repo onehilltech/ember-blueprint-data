70132adf909493635d4923a0678b5761f8aaed49
James Hill <hilljh82@gmail.com>
2024-04-27 08:53:43 -0400

    fix: fixed runtime error in update; refactored code

--- addon/-private/actions/search.js
+++ addon/-private/actions/search.js
@@ -2,35 +2,32 @@ import { assert } from '@ember/debug';
 
 import { normalizeResponseHelper } from './serializer-response';
 import { serializerForAdapter } from './serializers';
-import { gte } from 'ember-compatibility-helpers';
 
 export default function (adapter, store, modelName, query, recordArray, options, directives) {
   const modelClass = store.modelFor (modelName); // adapter.search needs the class
   let promise;
 
-  if (gte ('ember-data', '4.7.0')) {
+  if (!store.recordArrayManager.createAdapterPopulatedRecordArray) {
     recordArray = recordArray || store.recordArrayManager.createArray ({
       type: modelName,
       query
     });
+
+    promise = Promise.resolve().then (() => adapter.search (store, modelClass, query, recordArray, options, directives));
+  }
+  else if (adapter.search.length > 3 || (adapter.search.wrappedFunction && adapter.search.wrappedFunction.length > 3)) {
+    recordArray = recordArray || store.recordArrayManager.createAdapterPopulatedRecordArray (modelName, query);
+    promise = Promise.resolve ().then (() => adapter.search (store, modelClass, query, recordArray, options, directives));
   }
   else {
-    const createRecordArray = adapter.search.length > 3 || (adapter.search.wrappedFunction && adapter.search.wrappedFunction.length > 3);
-
-    if (createRecordArray) {
-      recordArray = recordArray || store.recordArrayManager.createAdapterPopulatedRecordArray (modelName, query);
-      promise = Promise.resolve().then (() => adapter.search (store, modelClass, query, recordArray, options, directives));
-    }
-    else {
-      promise = Promise.resolve().then (() => adapter.search (store, modelClass, query));
-    }
+    promise = Promise.resolve().then (() => adapter.search (store, modelClass, query));
   }
 
-  let label = `DS: Handle Adapter#search of ${modelName}`;
-  promise = guardDestroyedStore(promise, store, label);
+  //let label = `DS: Handle Adapter#search of ${modelName}`;
+  //promise = guardDestroyedStore(promise, store, label);
 
   return promise.then (adapterPayload => {
-    if (gte ('ember-data', '4.7.0')) {
+    if (!store.recordArrayManager.createAdapterPopulatedRecordArray) {
       const serializer = store.serializerFor (modelName);
       const payload = normalizeResponseHelper (serializer, store, modelClass, adapterPayload, null, 'query');
       const identifiers = store._push (payload);
